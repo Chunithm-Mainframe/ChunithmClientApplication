9599342f03adb76116d26f13396fedc87b3431f7
MAX-eipi <max.eipi.3141@gmail.com>
2021-08-29 00:52:59 +0900

    update test

--- Core.NET/BeatsmapConstIdentifier_UnitTest/DummyDataTest.cs
+++ Core.NET/BeatsmapConstIdentifier_UnitTest/DummyDataTest.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BeatsmapConstIdentifier;
+using ChunithmClientLibrary;
+using ChunithmClientLibrary.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using Core = ChunithmClientLibrary.Core;
+using MusicRating = BeatsmapConstIdentifier.MusicRating;
 
 namespace BeatsmapConstIdentifier_UnitTest
 {
@@ -8,7 +15,7 @@ namespace BeatsmapConstIdentifier_UnitTest
     {
         public void TestFile(string inputFileName, string outputFileName)
         {
-            System.Console.WriteLine($"{nameof(TestFile)}({inputFileName}, {outputFileName})");
+            Console.WriteLine($"{nameof(TestFile)}({inputFileName}, {outputFileName})");
 
             var actuals = TestUtility.TestFile(inputFileName).ToArray();
             var expecteds = TestUtility.ReadResourceFileLines(outputFileName).Select(x => int.Parse(x)).ToArray();
@@ -23,7 +30,7 @@ namespace BeatsmapConstIdentifier_UnitTest
                 .Select(x => $"{x.lineNumber}\nexpected: {x.expected}\nactual: {x.actual}\n")
                 .ToList();
 
-            diffs.ForEach(System.Console.WriteLine);
+            diffs.ForEach(Console.WriteLine);
 
             Assert.IsTrue(!diffs.Any());
         }
@@ -43,10 +50,97 @@ namespace BeatsmapConstIdentifier_UnitTest
         }
 
         [TestMethod]
-        public void Test_Error1()
+        public void Test1()
         {
-            var outputs = TestUtility.TestFile("input_error_01.txt");
-            Assert.IsFalse(outputs.Any());
+            (bool success, IReadOnlyList<MusicRating> musics) Run(MusicRepository repository, RatingRecordContainer container)
+            {
+                var input = new RunnerInput();
+                input.SetMusicRepository(repository);
+                input.AddRatingRecordContainer(container);
+                return Runner.Run(input);
+            }
+
+            const double expected1 = 12.0;
+            const double expected2 = 11.5;
+
+            void Compare(IReadOnlyList<MusicRating> musics, (int lowerLimit, int upperLimit) x1, (int lowerLimit, int upperLimit) x2)
+            {
+                Assert.AreEqual(x1.lowerLimit == x1.upperLimit, musics[0].Established);
+                Assert.AreEqual(x1.lowerLimit, musics[0].LowerLimit);
+                Assert.AreEqual(x1.upperLimit, musics[0].UpperLimit);
+
+                Assert.AreEqual(x2.lowerLimit == x2.upperLimit, musics[1].Established);
+                Assert.AreEqual(x2.lowerLimit, musics[1].LowerLimit);
+                Assert.AreEqual(x2.upperLimit, musics[1].UpperLimit);
+            }
+
+            var masterMusics = new List<MasterMusic>
+            {
+                new MasterMusic(1, string.Empty, string.Empty),
+                new MasterMusic(2, string.Empty, string.Empty),
+                new MasterMusic(3, string.Empty, string.Empty),
+                new MasterMusic(4, string.Empty, string.Empty),
+            };
+
+            var musicRatings = new List<Core.MusicRating>()
+            {
+                new Core.MusicRating(1, Difficulty.Invalid, Music.GetRangeBaseRating(expected1).lowerLimit, false),
+                new Core.MusicRating(2, Difficulty.Invalid, Music.GetRangeBaseRating(expected2).lowerLimit, false),
+                new Core.MusicRating(3, Difficulty.Invalid, 11.5, true),
+                new Core.MusicRating(4, Difficulty.Invalid, 11.0, false),
+            };
+
+            var musicRepository = new MusicRepository(masterMusics, musicRatings);
+
+            var ratingRecordContainer = new RatingRecordContainer();
+            {
+                // 変動なし
+
+                ratingRecordContainer.Records.Clear();
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 1000000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(2, Difficulty.Invalid, 1005000));
+
+                var (success, musics) = Run(musicRepository, ratingRecordContainer);
+                Assert.AreEqual(true, success);
+                Compare(musics, (1200, 1260), (1100, 1160));
+            }
+            {
+                // ID 1 の確定
+
+                ratingRecordContainer.Records.Clear();
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 1000000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(3, Difficulty.Invalid, 1005000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 999999));
+
+                var (success, musics) = Run(musicRepository, ratingRecordContainer);
+                Assert.AreEqual(true, success);
+                Compare(musics, (1200, 1200), (1100, 1160));
+            }
+            {
+                // 値域の更新テスト1
+
+                ratingRecordContainer.Records.Clear();
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 1000000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(2, Difficulty.Invalid, 1005000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 999999));
+
+                var (success, musics) = Run(musicRepository, ratingRecordContainer);
+                Assert.AreEqual(true, success);
+                Compare(musics, (1200, 1210), (1150, 1160));
+            }
+            {
+                // 値域の更新テスト2
+
+                ratingRecordContainer.Records.Clear();
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(4, Difficulty.Invalid, 1004500));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 1000000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(2, Difficulty.Invalid, 1005000));
+                ratingRecordContainer.Records.Add(new RatingRecordContainer.Reocrd(1, Difficulty.Invalid, 999999));
+
+                var (success, musics) = Run(musicRepository, ratingRecordContainer);
+                Assert.AreEqual(true, success);
+                Compare(musics, (1200, 1200), (1150, 1150));
+            }
         }
     }
 }
