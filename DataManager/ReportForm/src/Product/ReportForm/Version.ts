ef516ca318d189f42d74a42e337ec43de754c67e
MAX-eipi <max.eipi.3141@gmail.com>
2021-11-19 00:56:55 +0900

    update version

--- DataManager/ReportForm/src/Product/ReportForm/Version.ts
+++ DataManager/ReportForm/src/Product/ReportForm/Version.ts
@@ -1,5 +1,5 @@
 export class Version {
-    private static readonly _toolVersion = '0.5.2';
+    private static readonly _toolVersion = '0.5.3.0';
     public static get toolVersion() {
         return this._toolVersion;
     }
